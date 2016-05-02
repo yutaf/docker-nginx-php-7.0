1561663c81ff4bba8aa4b090ce15a09b675851a4
yutaf <yutafuji2008@gmail.com>
2016-05-02 19:39:50 +0900

    Update scripts/run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,8 @@
 
 # php session
 mkdir -m 777 -p /srv/php/session
-echo "tmpfs /srv/php/session tmpfs size=32m,mode=700,uid=apache,gid=apache 0 0" >> /etc/fstab
-mount /srv/php/session # need docker run "--cap-add=SYS_ADMIN" option
+echo "tmpfs /srv/php/session tmpfs size=32m,mode=700,uid=www-data,gid=www-data 0 0" >> /etc/fstab
+mount /srv/php/session # need docker to run with "--privileged" option
 # php upload_tmp_dir
 mkdir -m 777 -p /srv/php/upload_tmp_dir
 
@@ -20,4 +20,6 @@ mkdir -m 777 -p /srv/php/upload_tmp_dir
 #
 
 # supervisor
-/usr/bin/supervisord
+#/usr/bin/supervisord
+
+nginx -g 'daemon off;'
